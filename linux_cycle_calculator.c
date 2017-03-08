7838c32cb6587edf8f92171cc35457621febd6ee
mateo i <mateo i>
2017-03-08 12:51:09 +0000

    Added mul/add options to the linux counter

--- linux_cycle_calculator.c
+++ linux_cycle_calculator.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define ITERATIONS 10
 
@@ -9,9 +10,8 @@
  * for the given number of times, and stores the results in the given array,
  * which must be of (at least) the required size.
  */
-void calculate_time(int number_of_iterations, uint64_t* array) {
+void calculate_mul_time(int number_of_iterations, uint64_t* array) {
     uint64_t time0, time1;
-
     for (int i = 0; i < number_of_iterations; i++) {
         // Put some values in rbx, and rcx ready to multiply and call CPUID to synchronize CPU.
         asm("mov $0xAAAAAAAAAAAAAAAA, %rbx;"
@@ -29,10 +29,40 @@ void calculate_time(int number_of_iterations, uint64_t* array) {
         asm("mov %%rax, %0;" : "=r" (time1));
         asm("CPUID;"
             "CPUID;");
-        
+
         array[i] = time1 - time0;
     }
 }
+
+/*
+ * Calculates the clock cycles taken to perform a 64-bit addition
+ * for the given number of times, and stores the results in the given array,
+ * which must be of (at least) the required size.
+ */
+void calculate_add_time(int number_of_iterations, uint64_t* array) {
+    uint64_t time0, time1;
+    for (int i = 0; i < number_of_iterations; i++) {
+        // Put some values in rbx, and rcx ready to multiply and call CPUID to synchronize CPU.
+        asm("mov $0xAAAAAAAAAAAAAAAA, %rbx;"
+            "mov $0xDEADBEEFEBBED007, %rcx;"
+            "CPUID;"
+            "CPUID;"
+            "CPUID;");
+        // read the timing register and put it in time0.
+        asm("RDTSC;");
+        asm("mov %%rax, %0;" : "=r" (time0));
+        // Do a 64 bit multiplication -- the time this will take depends on the ALU load.
+        asm("add %rbx, %rcx;");
+        // Check the clock again, put it in time1, and synchronize the CPU.
+        asm("RDTSC;");
+        asm("mov %%rax, %0;" : "=r" (time1));
+        asm("CPUID;"
+            "CPUID;");
+
+        array[i] = time1 - time0;
+    }
+}
+
 
 int main(int argc, char** argv) {
     int iterations = ITERATIONS;
@@ -40,7 +70,11 @@ int main(int argc, char** argv) {
         iterations = atoi(argv[1]);
     }
     uint64_t values[iterations];
-    calculate_time(iterations, values);
+    if (argc >= 3 && strcmp(argv[2], "add")) {
+        calculate_add_time(iterations, values);
+    } else {
+        calculate_mul_time(iterations, values);
+    }
     for (int i = 0; i < iterations; i++) {
         printf("%lu\n", values[i]);
     }
